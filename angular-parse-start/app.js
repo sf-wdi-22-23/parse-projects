d7e9278e1513347449bd6cdc9a176c98018f86b6
Ben Hulan <ben.hulan@generalassemb.ly>
2015-12-08 00:38:59 -0800

    removes api keys

--- angular-parse-start/app.js
+++ angular-parse-start/app.js
@@ -1,6 +1,6 @@
 angular.module('parseSimple', [])
 .run(['$rootScope', function($rootScope){
     // initialize Parse with Application ID and Javascript Key
-    Parse.initialize("CCrrZJCx7GAwrKy3ybIJg5qDjpiZVZj3QYpXakzt", "GVy024wuNobVdxSg4cR9FyXPbeZFGTsn7C7eZPLy");
+    Parse.initialize("ApplicationId", "JavascriptKey");
     console.log('Parse is running.');
 }]);
